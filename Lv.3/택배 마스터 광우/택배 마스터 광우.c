df0c2e2c6a28dbfb9b1700cb7224b1b9d1546886
leejw0709@gmail.com <leejw0709@gmail.com>
2024-09-17 14:56:17 +0900

    -

---
+++ Lv.3/택배 마스터 광우/택배 마스터 광우.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <limits.h>
+
+int N, M, K;
+int line[50];
+int min = INT_MAX;
+
+void Swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void Permute(int *arr, int l, int r){
+    if(l == r){
+        int count = 0;
+        int weight = 0;
+        int total_weight = 0;
+        int index = 0;
+
+        while(1){
+            if(count == K){
+                index = 0;
+                break;
+            }
+
+            if(weight + arr[index] <= M){
+                weight += arr[index];
+                total_weight += arr[index];
+                index++;
+            }
+            else{
+                count++;
+                weight = 0;
+            }
+            if(index == N)
+                index = 0;
+        }
+        if(min > total_weight)
+            min = total_weight;
+    }
+    else{
+        for(int i = l; i <= r; i++){
+            Swap((arr + l), (arr + i));
+            Permute(arr, l + 1, r);
+            Swap((arr + l), (arr + i));
+        }
+    }
+}
+
+int main(){
+    scanf("%d %d %d", &N, &M, &K);
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &line[i]);
+    }
+
+    Permute(line, 0, N-1);
+
+    printf("%d\n", min);
+    return 0;
+}
+
