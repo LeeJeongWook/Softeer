93b7c61852c39b86efa55bd5163fe5c594fc3832
leejw0709@gmail.com <leejw0709@gmail.com>
2024-09-19 15:42:09 +0900

    -

--- Lv.3/Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.c
+++ Lv.3/Hanyang Popularity Exceeding Competition/Hanyang Popularity Exceeding Competition.c
@@ -1,8 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int N;
+#define MAX 200000
+
+int N, pop = 0;
+int Pi[MAX];
+int Ci[MAX];
 
 int main(){
+    scanf("%d", &N);
 
+    for(int i = 0; i < N; i++){
+        scanf("%d %d", &Pi[i], &Ci[i]);
+    }
+
+    for(int i = 0; i < N; i++){
+        if(abs(pop - Pi[i]) <= Ci[i]){
+            pop++;
+        }
+    }
+
+    printf("%d", pop);
+
+    return 0;
 }
