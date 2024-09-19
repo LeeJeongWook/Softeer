7e9792822774a1b1b0ef26bdf46ca57b59f96415
LeeJeongWook <leejw0709@gmail.com>
2024-09-19 13:26:01 +0900

    -

--- Lv.3/나무 조경/나무 조경.c
+++ Lv.3/나무 조경/나무 조경.c
@@ -1,56 +1,59 @@
-#define _CRT_SRCURE_NO_WARNINGS
 #include <stdio.h>
 
-int n, nx, ny, result = 0, max_sum = 0, max_count = 4;
+int N;
+int max_cnt;
+int result = 0;
 int forest[4][4];
-int visited[4][4] = {0,};
+int visited[4][4];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {1, -1, 0, 0};
 
-void DFS(int count, int sum){
-    if(count == max_count){
-        if(result < max_sum)
-            result = max_sum;
+void DFS(int cnt,int sum) {
+    if(cnt == max_cnt) {
+        if(result < sum)
+            result = sum;
+        return;
     }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(visited[i][j] == 0){
-                for(int k = 0; k < 4; k++){
-                    nx = j + dx[k];
-                    ny = i + dy[k];
-                    if(nx >= 0 && nx < n && ny >= 0 && ny < n){
-                        visited[i][j] = 1;
-                        DFS(count+1, sum + forest[i][j] + forest[ny][nx]);
-                        visited[i][j] = 0;
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if(visited[i][j] == 0) {
+                visited[i][j] = 1;
+                
+                for(int k = 0; k < 4; k++) {
+                    int nx = j + dx[k];
+                    int ny = i + dy[k];
+                    if((nx >= 0 && nx < N && ny >= 0 && ny < N) && (visited[ny][nx] == 0)) {
+                        visited[ny][nx] = 1;
+                        DFS(cnt + 1, sum + forest[i][j] + forest[ny][nx]);
+                        visited[ny][nx] = 0;
                     }
                 }
-                visited[i][j]=0;
+                visited[i][j] = 0;
             }
         }
     }
 }
-
-int main(){
-    scanf("%d", &n);
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+int main() {
+    
+    scanf("%d", &N);
+    
+    if(N == 2) {
+        max_cnt=2;
+    }
+    else {
+        max_cnt=4;
+    }
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
             scanf("%d", &forest[i][j]);
         }
     }
-
-    if(n == 2){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                result += forest[i][j];
-            }
-        }
-    }
-    else{
-        DFS(0, 0);
-    }
-
+    
+    DFS(0, 0);
+    
     printf("%d", result);
-
+    
     return 0;
 }
