12621289dbcecbfa7db7e97c4e2e3b5e01ae7729
leejw0709@gmail.com <leejw0709@gmail.com>
2024-09-21 11:13:03 +0900

    -

--- Lv.2/나무 공격/나무 공격.c
+++ Lv.2/나무 공격/나무 공격.c
@@ -1,47 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(){
-    int n, m, l1, l2, r1, r2, cnt = 0;
-    int field[100][100];
-    
+int n, m, l, r, cnt = 0;
+int field[100][100];
+
+int main(void){
     scanf("%d %d", &n, &m);
-    
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             scanf("%d", &field[i][j]);
-        }
-    }
-
-    scanf("%d %d", &l1, &r1);
-    scanf("%d %d", &l2, &r2);
-
-    for(int i = l1-1; i < r1; i++){
-        for(int j = 0; j < m; j++){
-            if(field[i][j] == 1){
-                field[i][j] = 0;
-                break;
-            }
-        }
-    }
-
-    for(int i = l2-1; i < r2; i++){
-        for(int j = 0; j < m; j++){
-            if(field[i][j] == 1){
-                field[i][j] = 0;
-                break;
-            }
-        }
-    }
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(field[i][j] == 1)
+            if(field[i][j])
                 cnt++;
         }
     }
 
-    printf("%d", cnt);
+    for(int i = 0; i < 2; i++){
+        scanf("%d %d", &l, &r);
+        for(int j = l - 1; j < r; j++){
+            for(int k = 0; k < m; k++){
+                if(field[j][k] == 1){
+                    field[j][k] = 0;
+                    cnt--;
+                    break;
+                }
+            }
+        }
+    }
 
+    printf("%d", cnt);
+    
     return 0;
 }
