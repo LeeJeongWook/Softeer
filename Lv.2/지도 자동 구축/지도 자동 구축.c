db0fdca50d378d8db67c6215718f6a52500cf44a
leejw0709@gmail.com <leejw0709@gmail.com>
2024-09-23 23:25:18 +0900

    -

--- Lv.2/지도 자동 구축/지도 자동 구축.c
+++ Lv.2/지도 자동 구축/지도 자동 구축.c
@@ -1,24 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(){
+int main(void){
     int N;
+    int T = 2;
+    int W = -1;
     int dot = 0;
-    int weight = 1;
-    int length = 2;
-
+    
     scanf("%d", &N);
 
-    for(int i = 0; i < N; i++){
-        length = length * 2 - 1;
-    }
+    for(int i = 0; i < N; i++)
+        T = T * 2 - 1;
 
-    for(int j = 0; j < length; j++){
-        dot += weight;
-        weight += 2;
+    for(int i = 0; i < T; i++){
+        W = W + 2;
+        dot += W;
     }
 
     printf("%d", dot);
-
+    
     return 0;
 }
