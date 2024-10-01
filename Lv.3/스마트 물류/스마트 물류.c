c9632db0bf488f8101e85f051ba3566963e27e17
leejw0709@gmail.com <leejw0709@gmail.com>
2024-10-01 22:59:20 +0900

    -

--- Lv.3/스마트 물류/스마트 물류.c
+++ Lv.3/스마트 물류/스마트 물류.c
@@ -1,61 +1,36 @@
 #include <stdio.h>
 
-int N, K;
+int N, K, ans = 0;
 int checked[20000];
-char line[20000];
+char line[20001];
 
 void Pick(int idx){
-    int cnt = K;
     for(int i = idx - K; i < idx; i++){
-        if(checked[i] == 0 && line[i] == 'H'){
+        if(i >= 0 && checked[i] == 0 && line[i] == 'H'){
             checked[i] = 1;
+            ans++;
             return;
         }
     }
-    for(int i = idx + K; i > idx; i--){
-        if(checked[i] == 0 && line[i] == 'H'){
+    for(int i = idx + 1; i <= idx + K; i++){
+        if(i < N && checked[i] == 0 && line[i] == 'H'){
             checked[i] = 1;
+            ans++;
             return;
         }
     }
-    // while(cnt){
-    //     i++;
-    //     if(checked[idx - i] = 0 && line[idx - i] == 'H'){
-    //         checked[idx - i] = 1;
-    //         break;
-    //     }
-    //     else if(checked[idx + i] = 0 && line[idx + i] == 'H'){
-    //         checked[idx + i] = 1;
-    //         break;
-    //     }
-    //     cnt--;
-    // }
-    return;
 }
 
 int main(){
     scanf("%d %d", &N, &K);
 
-    for(int i = 0; i < N; i++)
-        scanf(" %c", &line[i]);
+    scanf("%s", line);
     
     for(int i = 0; i < N; i++){
-        printf("line[%d]: %c\n",i, line[i]);
-    }
-    printf("\n\n");
-
-    
-    for(int i = 0; i < N; i++){
-        printf("i: %d\n", i);
-        if(line[i] == 'P'){
-            checked[i] = 1;
+        if(line[i] == 'P')
             Pick(i);
-        }
-        printf("checked: ");
-        for(int j = 0; j < N; j++){
-            printf("%d", checked[j]);
-        }
-        printf("\n");
     }
+
+    printf("%d", ans);
     return 0;
 }
